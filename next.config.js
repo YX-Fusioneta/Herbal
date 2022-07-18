d81813be8056fdeee5dbd8be5da22412af7652d9
YX-Fusioneta <goh.yee.xuan@fusioneta.com>
2022-07-18 22:15:52 +0800

    Add config

--- next.config.js
+++ next.config.js
@@ -5,19 +5,5 @@ const nextConfig = {
 
 }
 
-const withImages = require('next-images')
-module.exports = withImages()
-
-const withImages = require('next-images')
-module.exports = withImages({
-  webpack(config, options) {
-    return config
-  }
-})
-
-const withImages = require('next-images')
-module.exports = withImages({
-  inlineImageLimit: false
-})
 
 module.exports = nextConfig
